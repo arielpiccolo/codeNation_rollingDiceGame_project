28ec4b8e534977d10562755ec41cd834f3434c30
Ariel Piccolo <47399439+arielpiccolo@users.noreply.github.com>
2020-06-08 23:55:30 +0100

    update

--- script.js
+++ script.js
@@ -1,37 +1,61 @@
+const roll = document.querySelector('.roll');
+const winningNumber = 20;
+const dice = document.querySelector('.dice');
+const score = document.querySelector('.score')
+const reset = document.querySelector('.reset')
+const msg = document.querySelector('.msg')
+const badNum = 1;
+const diceMsg = 'You just rolled a # '
+const totalmsg = 'You scored  '
+const frame = document.getElementById('frame');
+let total = 0;
 
 
-let click = document.getElementsByClassName("pressDown");
+roll.addEventListener('click', () => {
+    let num = (Math.floor((Math.random() * 6) + 1))
+    dice.innerHTML = `${diceMsg} ${num} Dice`;
+    total += num;
+    score.innerHTML = `${totalmsg} ${total} Points! so far, keep going...`;
 
-let ranNum = Math.floor(Math.random() * 6);
+    if (num === badNum) {
+        msg.innerHTML = "You just rolled #1 Dice, you lose!     -       >>> hit Reset to start over <<<";
+        msg.style.color = 'red';
+        roll.style.visibility = 'hidden';
+        frame.src = './img/dice1.png';
+        
+    };
 
-
-
-/* functio that sends text to id score at the moment for texting */
-document.addEventListener('click', ()=> {
-    document.getElementById("score").innerHTML = ranNum; /* <- next add a random num gen here */  
-    let diceCount = document.getElementById("msgArea").innerHTML = ranNum;
-
-})
-
-/* create a function to pick the corresponding dice image depening on the random number */
-
-
-
-
-/* display the dice and the value */
-
+    if (total >= winningNumber) {
+        msg.innerHTML = "You scored over 20 points with out hitting the #1, you won!!!     -       >>> hit Reset to start over <<<";
+        roll.style.visibility = 'hidden';
+    };
 
 
+    if (num === 1) {
+        frame.src = './img/dice1.png';
+    } else if (num === 2) {
+        frame.src = './img/dice2.png';
+    } else if (num === 3) {
+        frame.src = './img/dice3.png';
+    } else if (num === 4) {
+        frame.src = './img/dice4.png';
+    } else if (num === 5) {
+        frame.src = './img/dice5.png';
+    } else {
+        frame.src = './img/dice6.png';
+        
+    };
 
-/* add a function to add the value of the consecutives rolled dices, update the total and display */
+});
 
 
 
-
-
-
-/* create a funtion to check if the total value reaches 20, if so the player wins,
-and if at any point the user get dice number 1 loses */
-
-
-/* add a reset button to star over */
+reset.addEventListener('click', () => {
+    roll.style.visibility = 'visible';
+    total = 0;
+    tempNumerHolder = 0;
+    dice.innerHTML = '0';
+    score.innerHTML = '0';
+    msg.innerHTML = '';
+    frame.src = '/img/dice0.png '
+});
